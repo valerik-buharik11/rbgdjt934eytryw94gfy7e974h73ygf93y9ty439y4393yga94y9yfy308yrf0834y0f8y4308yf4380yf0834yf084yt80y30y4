7fc934fc0c92009c02bb62f3bac3ec469cc680db
valerik-buharik11 <106351993+valerik-buharik11@users.noreply.github.com>
2023-05-04 21:23:14 +0300

    Add files via upload

---
+++ values.js
@@ -0,0 +1,24 @@
+// Активировать документы (true - включены, false - отключены)
+var isRightsEnabled = true; // Права
+var isCovidCertificateEnabled = false; // COVID-сертификат
+
+// Общее
+var birthdate = "14.02.2004"; // Дата рождения
+var fullname = "Мамаєв Валерій Дмитрович" // ФИО
+var name = "Валерій" // Имя
+
+// Паспорт
+var passport_id = "475743597" // Номер паспорта
+
+// КПП
+var kpp_id = "62056369662" // ИНН
+
+// Права
+var rights_valid_until = "22.12.2027" // Права "Дійсні до"
+var rights_categories = "A, B" // Права "Категорії"
+var rights_tsc = "ТСЦ 8631" // Права "Видав
+var rights_id = "KBE968639" // Права "Серія та номер""
+
+// COVID-сертификат
+var covid_valid_until = "11.04.2023" // Сертификат "Дійсний до"
+var covid_certificate_id = "URN:UVCI:01:UA:0<br>E55669376876888<br>B9E2520C4F88930" // Номер сертификата (<br> - перенос строки)
